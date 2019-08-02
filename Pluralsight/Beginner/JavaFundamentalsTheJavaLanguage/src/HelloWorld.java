e3b9bba1c289def4fbf81674ad8b359401ef664a
Nivali <nivali.m92@gmail.com>
2019-08-01 23:16:35 -0600

    First Commit. Hello World Program.

---
+++ Pluralsight/Beginner/JavaFundamentalsTheJavaLanguage/src/HelloWorld.java
@@ -0,0 +1,13 @@
+/*********************************************************************
+ * Class Name : HelloWorld
+ * Author     : Nivali
+ * Timestamp  : Aug 01 ,2019. 23:09
+ *********************************************************************/
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
